44c83c64f87877831179679407220c6bf894e7b7
tu201906115 <tu201906115@214-3>
2021-11-12 19:32:12 +0400

    Add project files.

---
+++ Files12112021/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace Files12112021
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //User user = new User();
+            //user.Name = "Martin";
+            //user.Surname = "Heidegger";
+            //user.Age = 77;
+
+            //Serialize(user, "Martin");
+            Console.WriteLine(DeSerialize("Martin"));
+        }
+        public static void Serialize(User user, string filename)
+        {
+            string path = @"C:\Users\tu201906115\Desktop\" + filename + ".txt";
+            Stream stream = new FileStream(path,FileMode.Create);
+            BinaryFormatter bf = new BinaryFormatter();
+            bf.Serialize(stream, user);
+        }
+
+
+        public static User DeSerialize(string filename)
+        {
+            string path = @"C:\Users\tu201906115\Desktop\" + filename + ".txt";
+            Stream stream = new FileStream(path, FileMode.Open);
+            BinaryFormatter bf = new BinaryFormatter();
+            var data = (User)bf.Deserialize(stream);
+            return data;
+        }
+        
+    }
+}
---
+++ Files12112021/User.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace Files12112021
+{
+    [Serializable]
+    class User:ISerializable
+    {
+        public User(string name, string surname, byte age)
+        {
+            Name = name;
+            Surname = surname;
+            Age = age;
+        }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public byte Age { get; set; }
+
+        public void GetObjectData(SerializationInfo info, StreamingContext context)
+        {
+            info.AddValue("Name", Name, typeof(string));
+            info.AddValue("Surname", Surname, typeof(string));
+            info.AddValue("Age", Age, typeof(byte));
+        }
+        public User()
+        {
+
+        }
+
+        public User(SerializationInfo info, StreamingContext context)
+        {
+            Name = (string)info.GetValue("Name", typeof(string));
+            Surname = (string)info.GetValue("Surname", typeof(string));
+            Age = (byte)info.GetValue("Age", typeof(byte));
+        }
+        public override string ToString()
+        {
+            return $"{Name} {Surname} - {Age}";
+        }
+    }
+   
+}
